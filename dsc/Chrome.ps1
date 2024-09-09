3c93f5b8f7a6a64bf6e181739c9efc4cef4fb8e1
david-rse <david.sanborn@render-se.com>
2024-09-09 11:30:39 -0700

    fix settings

--- dsc/Chrome.ps1
+++ dsc/Chrome.ps1
@@ -20,13 +20,13 @@ configuration Chrome
             # }
             OrgSettings = @{
                 'V-221563' = @{
-                    OptionValue = '' # a list of administrator-approved extension IDs
-                },
+                    ValueData = '' # a list of administrator-approved extension IDs
+                }
                 'V-221564' = @{
-                    OptionValue = 'Google Encrypted' # an organization approved encrypted search provider
-                },
+                    ValueData = 'Google Encrypted' # an organization approved encrypted search provider
+                }# a list of administrator-approved extension IDs
                 'V-221565' = @{
-                    OptionValue = 'https://www.google.com/search?q={searchTerms}' # an organization-approved encrypted search string
+                    ValueData = 'https://www.google.com/search?q={searchTerms}' # an organization-approved encrypted search string
                 }
             }
             # SkipRule = @(
@@ -36,10 +36,3 @@ configuration Chrome
 }
 
 Chrome
-
-# WARNING: Google_Chrome_Current_Windows: V-221563/RegistryRule/medium contains an empty Organizational Value, setting
-# rule as Skipped
-# WARNING: Google_Chrome_Current_Windows: V-221564/RegistryRule/medium contains an empty Organizational Value, setting
-# rule as Skipped
-# WARNING: Google_Chrome_Current_Windows: V-221565/RegistryRule/medium contains an empty Organizational Value, setting
-# rule as Skipped
